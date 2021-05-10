e575077fb650615c54673bcddeac68d51fcc33e0
LibraLF <1401780669@qq.com>
2021-05-10 12:57:15 +0800

    '购物车完善'

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from "./router";
 import store from "./store";
 import toast from "components/common/toast"
+import FastClick from "fastclick"
+import VueLazyLoad from "vue-lazyload"
 
 Vue.config.productionTip = false
 
@@ -10,6 +12,12 @@ Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 //安装toast插件
 Vue.use(toast)
+
+//解决移动端300ms延迟,attach附加
+FastClick.attach(document.body)
+
+//使用懒加载
+Vue.use(VueLazyLoad)
 
 new Vue({
   render: h => h(App),
